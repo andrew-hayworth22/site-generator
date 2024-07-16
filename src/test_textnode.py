c1e1c8712644e83d562631e964fed42f32356852
andrew-hayworth22 <mhaywort@kent.edu>
2024-07-15 20:23:05 -0400

    Added parsing of lines of text to text nodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,6 +4,7 @@ from textnode import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
     text_type_text,
     text_type_bold,
     text_type_italic,
@@ -246,6 +247,34 @@ class TestSplitLink(unittest.TestCase):
         ]
 
         self.assertListEqual(split_nodes_link(nodes), expected)
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_all_types(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+
+        result = text_to_textnodes(text)
+        self.assertListEqual(result, expected)
+
+    def test_no_splits(self):
+        text = "This is some text!"
+        expected = [
+            TextNode("This is some text!", text_type_text),
+        ]
+
+        result = text_to_textnodes(text)
+        self.assertListEqual(result, expected)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -42,12 +42,13 @@ class TextNode:
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
-        if old_node.text_type != "text":
+        if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
+            continue
 
         strings = old_node.text.split(delimiter)
         if len(strings) % 2 == 0:
-            raise Exception("Invalid Markdown syntax")
+            raise Exception("Invalid markdown: missing closing delimiter")
         
         for idx in range(len(strings)):
             if len(strings[idx]) == 0:
@@ -68,12 +69,17 @@ def split_nodes_image(old_nodes):
             new_nodes.append(old_node)
             continue
 
-        string = old_node.text
+        text = old_node.text
         for image in images:
-            split_strings = string.split(f"![{image[0]}]({image[1]})", 1)
-            new_nodes.append(TextNode(split_strings[0], text_type_text))
+            split_strings = text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(split_strings) != 2:
+                raise ValueError("Invalid markdown: malformed image")
+            if split_strings[0] != "":
+                new_nodes.append(TextNode(split_strings[0], text_type_text))
             new_nodes.append(TextNode(image[0], text_type_image, image[1]))
-            string = split_strings[1]
+            text = split_strings[1]
+        if text != "":
+            new_nodes.append(TextNode(text, text_type_text))
     return new_nodes
 
 def split_nodes_link(old_nodes):
@@ -84,10 +90,22 @@ def split_nodes_link(old_nodes):
             new_nodes.append(old_node)
             continue
 
-        string = old_node.text
+        text = old_node.text
         for link in links:
-            split_strings = string.split(f"[{link[0]}]({link[1]})", 1)
+            split_strings = text.split(f"[{link[0]}]({link[1]})", 1)
             new_nodes.append(TextNode(split_strings[0], text_type_text))
             new_nodes.append(TextNode(link[0], text_type_link, link[1]))
-            string = split_strings[1]
+            text = split_strings[1]
+        if text != "":
+            new_nodes.append(TextNode(text, text_type_text))
     return new_nodes
+
+def text_to_textnodes(text):
+    result = TextNode(text, text_type_text)
+    result = split_nodes_delimiter([result], "**", text_type_bold)
+    result = split_nodes_delimiter(result, "*", text_type_italic)
+    result = split_nodes_delimiter(result, "`", text_type_code)
+    result = split_nodes_image(result)
+    result = split_nodes_link(result)
+
+    return result
